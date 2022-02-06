3a07369cec4f14e8123fe427101c660d6a1c6c04
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-06 19:10:13 +0530

    Added FutureSwap Content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import "./App.css";
-import { BrowserRouter} from "react-router-dom";
+// import React from "react";
+// import Navbar from "./components/Navbar";
+// import "./App.css";
+// import { BrowserRouter} from "react-router-dom";
 
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+    Hiii
+      {/* <BrowserRouter>
         <Navbar />
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
 }
