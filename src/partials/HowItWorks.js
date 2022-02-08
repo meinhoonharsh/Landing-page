e33f583fc643ebaa631cbcc8d55b5fabef3c1b1e
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-09 01:10:05 +0530

    Created How It works Section

---
+++ src/partials/HowItWorks.js
@@ -0,0 +1,123 @@
+import React from "react";
+
+export default function HowItWorks() {
+  return (
+    <>
+      <div className="s2">
+        <div className="s2-wrapper">
+          <div
+            data-w-id="5b6da96c-f3bc-7ac5-0a8a-bf7907321711"
+            className="s2-info-container"
+          >
+            <h2 className="h2">Take full control of your crypto</h2>
+            <p className="p is-s2-p">
+              Built on Ethereum, our non-custodial perpetuals exchange focuses
+              on one thing: being the best execution environment for trades.
+            </p>
+          </div>
+          <div className="s2-blocks-wrapper">
+            <div
+              data-w-id="a07dd5fa-e232-2d4d-0204-e7a5e93200e2"
+              className="s2-blocks-container"
+            >
+              <div
+                data-w-id="0f9b7b89-f010-ae8c-147c-72ef0f4a1659"
+                className="s2-block"
+              >
+                <div className="s2-content-container">
+                  <div className="s2-blur-container">
+                    <div className="s2-blur" />
+                  </div>
+                  <div className="s2-icon-container">
+                    <img
+                      src="assets/img/icons/1.png"
+                      loading="lazy"
+                      alt=""
+                      className="s2-icon"
+                    />
+                  </div>
+                  <h3 className="s2-h3">
+                    We list high yielding real estate after due diligence
+                  </h3>
+                </div>
+              </div>
+              <div className="s2-block">
+                <div className="s2-content-container">
+                  <div className="s2-blur-container">
+                    <div className="s2-blur is-blur-2" />
+                  </div>
+                  <div className="s2-icon-container">
+                    <img
+                      src="assets/img/icons/2.png"
+                      loading="lazy"
+                      alt=""
+                      className="s2-icon is-icon-2"
+                    />
+                  </div>
+                  <h3 className="s2-h3">
+                    We tokenize property into NFTs, each representing fractional
+                    ownership
+                  </h3>
+                </div>
+              </div>
+              <div className="s2-block">
+                <div className="s2-content-container">
+                  <div className="s2-blur-container">
+                    <div className="s2-blur is-blur-3" />
+                  </div>
+                  <div className="s2-icon-container">
+                    <img
+                      src="assets/img/icons/3.png"
+                      loading="lazy"
+                      alt=""
+                      className="s2-icon is-icon-3"
+                    />
+                  </div>
+                  <h3 className="s2-h3">
+                    Get NFT's directly into your crypto wallet
+                  </h3>
+                </div>
+              </div>
+              <div className="s2-block">
+                <div className="s2-content-container">
+                  <div className="s2-blur-container">
+                    <div className="s2-blur is-blur-4" />
+                  </div>
+                  <div className="s2-icon-container">
+                    <img
+                      src="assets/img/icons/4.png"
+                      loading="lazy"
+                      alt=""
+                      className="s2-icon is-icon-4"
+                    />
+                  </div>
+                  <h3 className="s2-h3">
+                    Get regular rent payments into your crypto wallet
+                  </h3>
+                </div>
+              </div>
+              <div className="s2-block">
+                <div className="s2-content-container">
+                  <div className="s2-blur-container">
+                    <div className="s2-blur is-blur-5" />
+                  </div>
+                  <div className="s2-icon-container">
+                    <img
+                      src="assets/img/icons/5.png"
+                      loading="lazy"
+                      alt=""
+                      className="s2-icon is-icon-5"
+                    />
+                  </div>
+                  <h3 className="s2-h3">
+                    Use our liquidity pools to borrow, lend and swap
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
