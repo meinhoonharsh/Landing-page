08d68b56aa66def425078c0eb60f4a23137dfad5
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-09 02:52:57 +0530

    Added Container in Products

--- src/partials/ProductSection.js
+++ src/partials/ProductSection.js
@@ -19,6 +19,21 @@ export default function ProductSection() {
                 <div className="s1-gradient-outline" />
               </div>
             </div>
+          </div>
+          <div className="s1-container">
+            <div className="s1-block-container">
+              <div className="s1-block">
+                <div className="s1-content-container">
+                  <h3 className="h3">All-time trade volume</h3>
+                  <div className="s1-numbers">
+                    $<span className="counter">4,200,368,117</span>
+                  </div>
+                </div>
+                <div className="s1-gradient-glow" />
+                <div className="s1-gradient-mask" />
+                <div className="s1-gradient-outline" />
+              </div>
+            </div>
             <div className="s1-block-container is-total-trades-block-container">
               <div className="s1-block">
                 <div className="s1-content-container">
