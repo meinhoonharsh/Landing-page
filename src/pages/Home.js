1c703538d56b27bbed39f39f9bbc405e1dfa3f71
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-09 01:07:37 +0530

    Created ProductSection PArtial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,38 +8,7 @@ export default function Home() {
       <div className="page-wrap">
        <Navbar />
         <Landing />
-        <div className="s1">
-          <div className="s1-wrapper">
-            <div className="s1-container">
-              <div className="s1-block-container">
-                <div className="s1-block">
-                  <div className="s1-content-container">
-                    <h3 className="h3">All-time trade volume</h3>
-                    <div className="s1-numbers">
-                      $<span className="counter">4,200,368,117</span>
-                    </div>
-                  </div>
-                  <div className="s1-gradient-glow" />
-                  <div className="s1-gradient-mask" />
-                  <div className="s1-gradient-outline" />
-                </div>
-              </div>
-              <div className="s1-block-container is-total-trades-block-container">
-                <div className="s1-block">
-                  <div className="s1-content-container">
-                    <h3 className="h3 is-green-h3">Total Trades</h3>
-                    <div className="s1-numbers">
-                      <span className="counter">10,434</span>
-                    </div>
-                  </div>
-                  <div className="s1-gradient-glow is-green-gradient-glow" />
-                  <div className="s1-gradient-mask" />
-                  <div className="s1-gradient-outline" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+       
         <div className="s2">
           <div className="s2-wrapper">
             <div
