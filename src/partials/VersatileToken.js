fc116300941a40f2b6f2742d5fba8238766eff0a
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-10 17:33:38 +0530

    Created Versatile Token Partial

---
+++ src/partials/VersatileToken.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+export default function VersatileToken() {
+  return (
+    <>
+      <div className="s6">
+        <div className="s6-container">
+          <div
+            data-w-id="649dbab4-e00a-e03a-732f-5ed95dca54ab"
+            className="s6-info-container"
+          >
+            <h2 className="h2 is-small-h2">
+              Come for the memes stay for the protocol.
+            </h2>
+            <p className="p is-s6-p">
+              Sure, having the best price execution on the market is great. But
+              is it better than having a Discord server full of spicy memes?
+              We’re not sure.
+              <br />‍<br />
+              Join our community of meme-loving masterminds and get the inside
+              scoop on what’s coming down the pipeline.
+            </p>
+            <a
+              href="https://discord.com/invite/gRtqvfqW4V"
+              className="filled-button w-button"
+            >
+              I need memes
+            </a>
+          </div>
+          <div className="s6-image-wrapper">
+            <div
+              data-w-id="13a914f3-4ae0-191c-55b4-82b0730137d9"
+              className="s6-image-container"
+            >
+              <img
+                src="images/Graphic-Meme-Cat.png"
+                loading="lazy"
+                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
+                srcSet="
+            images/Graphic-Meme-Cat-p-800.png   800w,
+            images/Graphic-Meme-Cat-p-1080.png 1080w,
+            images/Graphic-Meme-Cat.png        1412w
+          "
+                alt=""
+                className="s6-cat-image"
+              />
+              <img
+                src="images/Graphic-Meme-Message.png"
+                loading="lazy"
+                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
+                srcSet="
+            images/Graphic-Meme-Message-p-500.png   500w,
+            images/Graphic-Meme-Message-p-800.png   800w,
+            images/Graphic-Meme-Message-p-1080.png 1080w,
+            images/Graphic-Meme-Message.png        1412w
+          "
+                alt=""
+                className="s6-message-image"
+              />
+              <img
+                src="images/Graphic-Meme-Card.png"
+                loading="lazy"
+                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
+                srcSet="
+            images/Graphic-Meme-Card-p-500.png   500w,
+            images/Graphic-Meme-Card-p-800.png   800w,
+            images/Graphic-Meme-Card-p-1080.png 1080w,
+            images/Graphic-Meme-Card.png        1412w
+          "
+                alt=""
+                className="s6-card-image"
+              />
+              <img
+                src="images/Graphic-Meme-Rocket.png"
+                loading="lazy"
+                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
+                srcSet="
+            images/Graphic-Meme-Rocket-p-500.png  500w,
+            images/Graphic-Meme-Rocket-p-800.png  800w,
+            images/Graphic-Meme-Rocket.png       1412w
+          "
+                alt=""
+                className="s6-rocket-image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
