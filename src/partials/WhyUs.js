0124a5fdb7f20a4dcd708ac2e7a4390d3b646b55
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-10 17:19:57 +0530

    Created WhyUs Section

---
+++ src/partials/WhyUs.js
@@ -0,0 +1,119 @@
+import React from "react";
+
+export default function WhyUs() {
+  return (
+    <>
+      <div className="s3">
+        <div className="s3-container">
+          <div className="s3-image-wrapper">
+            <div className="s3-image-container">
+              <img
+                src="images/Graphic-Decentralized-Box.png"
+                loading="lazy"
+                data-w-id="16e7b7c0-73b3-38ef-b935-fa27dc948b0c"
+                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
+                srcSet="
+            images/Graphic-Decentralized-Box-p-500.png   500w,
+            images/Graphic-Decentralized-Box-p-800.png   800w,
+            images/Graphic-Decentralized-Box-p-1080.png 1080w,
+            images/Graphic-Decentralized-Box.png        1117w
+          "
+                alt=""
+                className="s3-box-image"
+              />
+              <img
+                src="images/Graphic-Decentralized-Base.png"
+                loading="lazy"
+                data-w-id="2a136f4b-ce37-a8da-4149-6d5b14ea3029"
+                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
+                srcSet="
+            images/Graphic-Decentralized-Base-p-500.png   500w,
+            images/Graphic-Decentralized-Base-p-800.png   800w,
+            images/Graphic-Decentralized-Base-p-1080.png 1080w,
+            images/Graphic-Decentralized-Base.png        1124w
+          "
+                alt=""
+                className="s3-base-image"
+              />
+              <img
+                src="images/Graphic-Decentralized-Orbitals.png"
+                loading="lazy"
+                data-w-id="dd382521-3a47-1d9a-0f32-82e357f71315"
+                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
+                srcSet="
+            images/Graphic-Decentralized-Orbitals-p-500.png   500w,
+            images/Graphic-Decentralized-Orbitals-p-800.png   800w,
+            images/Graphic-Decentralized-Orbitals-p-1080.png 1080w,
+            images/Graphic-Decentralized-Orbitals.png        1121w
+          "
+                alt=""
+                className="s3-orbitals-image"
+              />
+            </div>
+          </div>
+          <div
+            data-w-id="dd382521-3a47-1d9a-0f32-82e357f71309"
+            className="s3-info-container"
+          >
+            <h2 className="h2 is-small-h2">
+              Fully decentralized. Completely secure.
+            </h2>
+            <p className="p is-s3-p">
+              We have all been in this industry too long not to make the
+              security of your funds our absolute top priority. &nbsp;That’s
+              why, so far, we’ve spent upwards of $450k on audits from Open
+              Zeppelin and Trail of Bits, as well as code reviews from white
+              hats.
+            </p>
+            <div className="s3-list-container">
+              <div className="s3-list-item">
+                <img
+                  src="images/Icon-Reviews.svg"
+                  loading="lazy"
+                  alt=""
+                  className="s3-icon"
+                />
+                <div className="s3-item-description">
+                  Constant reviews and testing of our platform
+                </div>
+              </div>
+              <div className="s3-list-item">
+                <img
+                  src="images/Icon-Secure.svg"
+                  loading="lazy"
+                  alt=""
+                  className="s3-icon"
+                />
+                <div className="s3-item-description">
+                  Your funds are as secure as possible
+                </div>
+              </div>
+              <div className="s3-list-item">
+                <img
+                  src="images/Icon-Decentralized.svg"
+                  loading="lazy"
+                  alt=""
+                  className="s3-icon"
+                />
+                <div className="s3-item-description">
+                  Fully decentralized and non-custodial
+                </div>
+              </div>
+              <div className="s3-list-item">
+                <img
+                  src="images/Icon-Experience.svg"
+                  loading="lazy"
+                  alt=""
+                  className="s3-icon"
+                />
+                <div className="s3-item-description">
+                  Built by seasoned crypto veterans
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
