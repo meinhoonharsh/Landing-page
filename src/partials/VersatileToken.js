a3947813dd8d0fc2ce0bce0617e35f8d26328cf5
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-10 21:06:36 +0530

    Added Illustration in Versatile Token Partial

--- src/partials/VersatileToken.js
+++ src/partials/VersatileToken.js
@@ -9,15 +9,15 @@ export default function VersatileToken() {
             data-w-id="649dbab4-e00a-e03a-732f-5ed95dca54ab"
             className="s6-info-container"
           >
-            <h2 className="h2 is-small-h2">
-                A Versatile Token
-            </h2>
+            <h2 className="h2 is-small-h2">A Versatile Token</h2>
             <p className="p is-s6-p">
-            Our on-platform liquidity pools let you seamlessly borrow, lend or swap your Real Estate Tokens.
-            Our on-platform liquidity pools let you seamlessly borrow, lend or swap your Real Estate Tokens.
+              Our on-platform liquidity pools let you seamlessly borrow, lend or
+              swap your Real Estate Tokens. Our on-platform liquidity pools let
+              you seamlessly borrow, lend or swap your Real Estate Tokens.
             </p>
             <p className="p is-s6-p">
-            Our on-platform liquidity pools let you seamlessly borrow, lend or swap your Real Estate Tokens.
+              Our on-platform liquidity pools let you seamlessly borrow, lend or
+              swap your Real Estate Tokens.
             </p>
             <a
               href="https://discord.com/invite/gRtqvfqW4V"
@@ -27,59 +27,14 @@ export default function VersatileToken() {
             </a>
           </div>
           <div className="s6-image-wrapper">
-            <div
-              data-w-id="13a914f3-4ae0-191c-55b4-82b0730137d9"
-              className="s6-image-container"
-            >
+            <div>
               <img
-                src="images/Graphic-Meme-Cat.png"
-                loading="lazy"
-                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
-                srcSet="
-            images/Graphic-Meme-Cat-p-800.png   800w,
-            images/Graphic-Meme-Cat-p-1080.png 1080w,
-            images/Graphic-Meme-Cat.png        1412w
-          "
-                alt=""
-                className="s6-cat-image"
-              />
-              <img
-                src="images/Graphic-Meme-Message.png"
-                loading="lazy"
-                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
-                srcSet="
-            images/Graphic-Meme-Message-p-500.png   500w,
-            images/Graphic-Meme-Message-p-800.png   800w,
-            images/Graphic-Meme-Message-p-1080.png 1080w,
-            images/Graphic-Meme-Message.png        1412w
-          "
-                alt=""
-                className="s6-message-image"
-              />
-              <img
-                src="images/Graphic-Meme-Card.png"
-                loading="lazy"
-                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
-                srcSet="
-            images/Graphic-Meme-Card-p-500.png   500w,
-            images/Graphic-Meme-Card-p-800.png   800w,
-            images/Graphic-Meme-Card-p-1080.png 1080w,
-            images/Graphic-Meme-Card.png        1412w
-          "
-                alt=""
-                className="s6-card-image"
-              />
-              <img
-                src="images/Graphic-Meme-Rocket.png"
-                loading="lazy"
-                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
-                srcSet="
-            images/Graphic-Meme-Rocket-p-500.png  500w,
-            images/Graphic-Meme-Rocket-p-800.png  800w,
-            images/Graphic-Meme-Rocket.png       1412w
-          "
-                alt=""
-                className="s6-rocket-image"
+                src="assets/img/vt.png"
+                className="s3-box-image"
+                style={{
+                  width: "85%",
+                  margin: "auto",
+                }}
               />
             </div>
           </div>
