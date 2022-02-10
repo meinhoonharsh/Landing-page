9d47457569a0063f6125e0182ad34782a027cb9b
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-10 17:41:56 +0530

    Created Footer Partial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,63 +16,6 @@ export default function Home() {
         <HowItWorks />
         <WhyUs />
         <VersatileToken />
-
-        <div className="footer">
-          <div className="footer-container">
-            <div className="footer-legal">
-              Copyright ©<span className="copyright-year">year</span> Futureswap
-              <br />
-              All rights reserved &nbsp;| &nbsp;
-              <a href="#" target="_blank" className="privacy-link">
-                Privacy
-              </a>
-            </div>
-            <img
-              src="images/Logo-Futureswap.svg"
-              loading="lazy"
-              alt=""
-              className="footer-logo"
-            />
-            <div className="social-container">
-              <a
-                href="https://twitter.com/futureswapx"
-                target="_blank"
-                className="social-holder w-inline-block"
-              >
-                <img
-                  src="images/Icon-Twitter-Gray.svg"
-                  loading="lazy"
-                  alt=""
-                  className="gray-social"
-                />
-                <img
-                  src="images/Icon-Twitter-Green.svg"
-                  loading="lazy"
-                  alt=""
-                  className="green-social"
-                />
-              </a>
-              <a
-                href="https://discord.com/invite/gRtqvfqW4V"
-                target="_blank"
-                className="social-holder w-inline-block"
-              >
-                <img
-                  src="images/Icon-Discord-Gray.svg"
-                  loading="lazy"
-                  alt=""
-                  className="gray-social"
-                />
-                <img
-                  src="images/Icon-Discord-Green.svg"
-                  loading="lazy"
-                  alt=""
-                  className="green-social"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
