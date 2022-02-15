f640aeb14bf18058dfdccb1566e0289c24d40507
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-15 22:32:40 +0530

    Applied Styling to Roadmap

--- src/partials/Roadmap.js
+++ src/partials/Roadmap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/Roadmap.scss";
 
 export default function Roadmap() {
   const Initiatives = [
@@ -6,43 +7,45 @@ export default function Roadmap() {
     [
       "Q1 2022",
       "Market validation, community building, MVP, fundraising",
-      "primaryblue"
+      "pink"
     ],
     [
       "Q2 2022",
       "Product Launch, First Property Listing, pre-ICO/ICO launch, legal structuring",
-      "primaryblue"
+      "turqoise"
     ],
     [
       "Q3 2022",
       "Partnerships, Pipeline of projects, adding new features like NFT liquidity pools",
-      "primaryblue"
+      "yaleblue"
     ]
   ];
   return (
     <div className="roadmap">
-      {Initiatives.map((initiative, index) => {
-        return (
-          <a href="" class={"initiativediv  " + initiative[2]}>
-            <div class=" iniiconbox">
-              <img
-                src={"/assets/img/icons/" + index + ".png"}
-                style={{
-                  position: "relative",
-                  transition: "0.5s ease",
-                  width: "100%"
-                }}
-                alt=""
-              />
-              <div class="shadowdiv"></div>
+      <div class="initiativescontainer">
+        {Initiatives.map((initiative, index) => {
+          return (
+            <div class={"initiativediv  " + initiative[2]}>
+              <div class=" iniiconbox">
+                <img
+                  src={"/assets/img/icons/" + (index+1) + ".png"}
+                  style={{
+                    position: "relative",
+                    transition: "0.5s ease",
+                    width: "100%"
+                  }}
+                  alt=""
+                />
+                <div class="shadowdiv"></div>
+              </div>
+              <div class="iniinfo">
+                <h4>{initiative[0]}</h4>
+                <p>{initiative[1]}</p>
+              </div>
             </div>
-            <div class="iniinfo">
-              <h4>{initiative[0]}</h4>
-              <p>{initiative[1]}</p>
-            </div>
-          </a>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
