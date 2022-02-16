83dc46cc6a68e76de38d33538cc5b0ce7d1fcc37
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-16 12:43:04 +0530

    Changed Layout of WhyUs Section

--- src/partials/WhyUs.js
+++ src/partials/WhyUs.js
@@ -1,50 +1,11 @@
 import React from "react";
+import "./styles/WhyUs.scss";
 
 export default function WhyUs() {
   return (
     <>
       <div className="s3 whyus">
         <div className="s3-container">
-          <div className="s3-image-wrapper">
-            <div className="">
-              <img
-                src="assets/img/wu.png"
-                className="s3-box-image"
-                style={{
-                  width: "75%",
-                  margin:'auto'
-                }}
-              />
-              {/* <img
-                src="images/Graphic-Decentralized-Base.png"
-                loading="lazy"
-                data-w-id="2a136f4b-ce37-a8da-4149-6d5b14ea3029"
-                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
-                srcSet="
-            images/Graphic-Decentralized-Base-p-500.png   500w,
-            images/Graphic-Decentralized-Base-p-800.png   800w,
-            images/Graphic-Decentralized-Base-p-1080.png 1080w,
-            images/Graphic-Decentralized-Base.png        1124w
-          "
-                alt=""
-                className="s3-base-image"
-              />
-              <img
-                src="images/Graphic-Decentralized-Orbitals.png"
-                loading="lazy"
-                data-w-id="dd382521-3a47-1d9a-0f32-82e357f71315"
-                sizes="(max-width: 479px) 90vw, (max-width: 991px) 72vw, (max-width: 1279px) 41vw, (max-width: 1439px) 42vw, 598px"
-                srcSet="
-            images/Graphic-Decentralized-Orbitals-p-500.png   500w,
-            images/Graphic-Decentralized-Orbitals-p-800.png   800w,
-            images/Graphic-Decentralized-Orbitals-p-1080.png 1080w,
-            images/Graphic-Decentralized-Orbitals.png        1121w
-          "
-                alt=""
-                className="s3-orbitals-image"
-              /> */}
-            </div>
-          </div>
           <div
             data-w-id="dd382521-3a47-1d9a-0f32-82e357f71309"
             className="s3-info-container"
