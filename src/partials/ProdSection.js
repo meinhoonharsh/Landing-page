bdc510c875a8e950ea222a34ee73e6e9ab61da48
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-16 13:41:39 +0530

    Created ProdSection Component

---
+++ src/partials/ProdSection.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function ProdSection() {
+  return (
+    <>
+      <div className="prodsection">
+          <div className="sidediv left">
+              <h3>MarketPlace</h3>
+              <span>Choose from a variety of listed NFTs</span>
+          </div>
+          <div className="centerdiv">
+              <h2>upto <span>16%</span> IRR</h2>
+              <span>Earn from regular rent and price appreciation</span>
+          </div>
+          <div className="sidediv right">
+            <h3>Regular Returns</h3>
+            <span>Earn from regular rent and price appreciation</span>
+          </div>
+      </div>
+    </>
+  );
+}
