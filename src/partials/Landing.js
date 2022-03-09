7d8db32a2716a3f07ef7b5611a317716ee9e9046
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-09 23:34:09 +0530

    Updated Landing Image

--- src/partials/Landing.js
+++ src/partials/Landing.js
@@ -34,9 +34,6 @@ export default function Landing() {
         <div className="waves-wrapper">
           <div className="waves-container">
             <img src="assets/img/landing.png"  />
-            <img src="assets/img/landing-brain.png" className="landing-brain" />
-            <img src="assets/img/landing-bitcoin.png" className="landing-bitcoin" />
-            <img src="assets/img/landing-humans.png" className="landing-humans" />
             
             {/* <img
                 src="images/Graphic-Hero-Blur.jpg"
