cbe690aa61fd2178d319e997f9f8750251dc7095
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-12 17:16:01 +0530

    Implemented Routing on HomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/realnfts">
+            <h2>Real NFTs</h2>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
