f0b6a4530b4f605d1afde52c4cb345599bc620c5
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-12 17:29:00 +0530

    Created Real NFTs Layout

--- src/pages/RealNFTs.js
+++ src/pages/RealNFTs.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import Footer from "../partials/Footer";
+import Navbar from "../partials/Navbar";
 
 export default function RealNFTs() {
   return (
-    <div>RealNFTs</div>
-  )
+    <>
+      <div className="page-wrap">
+        <Navbar />
+        Real NFTs Page
+        <Footer />
+      </div>
+    </>
+  );
 }
