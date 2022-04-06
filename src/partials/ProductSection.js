160316977144b7ee6039d5158062c1e4c90e8bad
Rounak Dubey <rkdy49@gmail.com>
2022-04-06 14:07:11 +0530

    Update ProductSection.js

--- src/partials/ProductSection.js
+++ src/partials/ProductSection.js
@@ -26,7 +26,7 @@ export default function ProductSection() {
                 <div className="s1-content-container">
                   <h3 className="h3">Marketplace</h3>
                   <div className="s1-numbers">
-                    <span className="counter">Choose from a variety of listed NFTs</span>
+                    <span className="counter">Choose from a variety of listed Tokens</span>
                   </div>
                 </div>
                 <div className="s1-gradient-glow" />
