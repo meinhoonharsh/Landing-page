a93eed09df62330e9142eaa8cc816858c766d9dc
meinhoonharsh <harshprogrammer782@gmail.com>
2022-10-01 07:26:43 +0530

    Fixed Responsiveness of Seller and Buyer Sections

--- src/partials/Seller.js
+++ src/partials/Seller.js
@@ -9,14 +9,22 @@ export default function VersatileToken() {
             data-w-id="649dbab4-e00a-e03a-732f-5ed95dca54ab"
             className="s6-info-container"
           >
-            <h2 className="h2 is-small-h2">Wanna sell your NFT? <br/>Get more out of your Sale!</h2>
+            <h2 className="h2 is-small-h2">
+              Wanna sell your NFT? <br />
+              Get more out of your Sale!
+            </h2>
             <p className="p is-s6-p">
-              List your NFT for sale with suitable terms(APR, duration of repayment) and choose the best deal offered.<br/>
-              Get 30% value right away.<br/>
-              Best case scenario... earn exciting interest on your sale.<br/>
-              In case of default, you get your NFT back and keep the premium amount. <br/><br/>
+              List your NFT for sale with suitable terms(APR, duration of
+              repayment) and choose the best deal offered.
+              <br />
+              Get 30% value right away.
+              <br />
+              Best case scenario... earn exciting interest on your sale.
+              <br />
+              In case of default, you get your NFT back and keep the premium
+              amount. <br />
+              <br />
               Enjoy increased liquidity due to increased buying potential.
-              
             </p>
 
             {/* <a href="#" className="filled-button w-button">
@@ -27,9 +35,8 @@ export default function VersatileToken() {
             <div>
               <img
                 src="assets/img/borrow1.png"
-                className="s3-box-image"
                 style={{
-                  width: "75%",
+                  width: "85%",
                   margin: "auto",
                 }}
               />
